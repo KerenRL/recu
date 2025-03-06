91a9ca39544e4ec675fe8282b379ac77ec3123b4
KerenRL <kl236704@gmail.com>
2025-03-06 16:33:21 -0600

    Cambio de repo

--- main.go
+++ main.go
@@ -18,19 +18,16 @@ func main() {
 		log.Fatalf("Error al inicializar Tienda: %v", err)
 	}
 
-	// Configurar rutas para perfumes
 	perfumeRouter := infraestructure.SetupRouter(infraestructure.NewMySQL())
 	for _, route := range perfumeRouter.Routes() {
 		r.Handle(route.Method, route.Path, route.HandlerFunc)
 	}
 
-	// Configurar rutas para tiendas
 	tiendaRouter := tiendaInfra.SetupRouter(tiendaInfra.NewMySQL())
 	for _, route := range tiendaRouter.Routes() {
 		r.Handle(route.Method, route.Path, route.HandlerFunc)
 	}
 
-	// Configurar proxies confiables (opcional)
 	r.SetTrustedProxies([]string{"127.0.0.1"})
 
 	if err := r.Run(":8080"); err != nil {
