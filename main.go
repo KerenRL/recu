b794ea7b0f2258de68633ed494030344a117066e
KerenRL <kl236704@gmail.com>
2025-04-07 22:14:08 -0600

    Mensaje de confirmación

--- main.go
+++ main.go
@@ -11,25 +11,21 @@ import (
 func main() {
 	r := gin.Default()
 
-	// Inicializar perfumes
 	if err := infraestructure.InitPerfume(); err != nil {
 		log.Fatalf("Error al inicializar Perfume: %v", err)
 	}
 
-	// Inicializar tiendas
 	if err := tiendaInfra.InitTienda(); err != nil {
 		log.Fatalf("Error al inicializar Tienda: %v", err)
 	}
 
-	// Cambiar esta línea
-	perfumeRepo := infraestructure.NewMySQL() // Correcto
+	perfumeRepo := infraestructure.NewMySQL() 
 	perfumeRouter := infraestructure.SetupRouter(perfumeRepo)
 	routes := perfumeRouter.Routes()
 	for _, route := range routes {
 		r.Handle(route.Method, route.Path, route.HandlerFunc)
 	}
 
-	// Configurar rutas de tiendas
 	tiendaRouter := tiendaInfra.SetupRouter(tiendaInfra.NewMySQL())
 	for _, route := range tiendaRouter.Routes() {
 		r.Handle(route.Method, route.Path, route.HandlerFunc)
